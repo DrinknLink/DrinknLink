3ca3e644a798cd0dc7cd69c7b427fff0e73f3d27
dsagud <dsagud@msn.com>
2013-06-10 10:21:16 +0200

    new change 1

---
+++ JavaApplication4/src/MainClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Sagud
+ */
+public class MainClass {
+
+    /**
+     * @param
+     * args
+     * the
+     * command
+     * line
+     * arguments
+     */
+    public static void main(String[] args) {
+        
+        // Dario's change 1
+        
+        
+    }
+}
