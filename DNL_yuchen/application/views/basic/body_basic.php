bc45f2341e62886134b2512d62987ef8ef05d6f2
dsagud <dsagud@msn.com>
2013-06-11 23:37:09 +0200

    Uploaded basic page

--- DNL_yuchen/application/views/basic/body_basic.php
+++ DNL_yuchen/application/views/basic/body_basic.php
@@ -1,16 +1,112 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<div id="background">
+    <div id="layer-gradient"></div>
+    <!--<div id="layer-header"></div>-->
+    <div id="layer-bubbles"></div>
+</div>
+
+<!-- Locale -->
+<div id="header">
+    <div id="locale">
+        <div class="column-center">						
+            <ul id="locale-selector-list" class="dnl-ui-list vertical">
+                <li class=""><span class="dnl-ui-sprite nl-NL"></span><a href="/locale/nl_NL">nederlands</a></li>
+                <li class="divider">|</li>
+                <li class="active"><span class="dnl-ui-sprite en-US"></span><a href="/locale/en_US">english</a></li>
+            </ul>
+        </div>			
+    </div>
+    <p style="margin-left: 72px;">H E A D I N G</p>
+</div>
+
+<div class="container">
+
+    <div class="column column-1-1">
+        <div class="row">
+            <div id="navDiv" class="dnl-ui-drop-shadow dnl-ui-rounded-corners">								
+                <div>
+                    <h1>Navigation</h1>
+
+                    <div class="tabbable">
+                        <ul class="nav nav-tabs">                              
+                            <li class="bigNavLinks active">
+                                <a href="#tab1" data-toggle="tab">
+                                    <table>
+                                        <tr>
+                                            <td><i class="icon-cog"></i></td>
+                                            <td>&nbsp General</td>
+                                        </tr>
+                                    </table>
+                                </a>
+                            </li>
+                            <li class="bigNavLinks">
+                                <a href="#tab2" data-toggle="tab">
+                                    <table>
+                                        <tr>
+                                            <td><i class="icon-lock"></i></td>
+                                            <td>&nbsp Security</td>
+                                        </tr>
+                                    </table>
+                                </a>
+                            </li>
+                            <li class="bigNavLinks">
+                                <a href="#tab3" data-toggle="tab">
+                                    <table>
+                                        <tr>
+                                            <td><i class="icon-eye-open"></i></td>
+                                            <td>&nbsp Privacy</td>
+                                        </tr>
+                                    </table>
+                                </a>
+                            </li>
+                            <li class="bigNavLinks">
+                                <a href="#tab4" data-toggle="tab">
+                                    <table>
+                                        <tr>
+                                            <td><i class="icon-shopping-cart"></i></td>
+                                            <td>&nbsp Payments</td>
+                                        </tr>
+                                    </table> 
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>	
+            </div>
+        </div>
+    </div>
+
+    <div class="column column-1-2">				
+        <div class="row">
+
+            <div id="basicDiv" class="dnl-ui-drop-shadow dnl-ui-rounded-corners">								
+                <div class="tab-content eventsTabContent">
+
+                    <div class="tab-pane active" id="tab1">
+                        <h1>General settings</h1>
+                    </div>
+                    <div class="tab-pane" id="tab2">
+                        <h1>Security settings</h1>
+                    </div>
+                    <div class="tab-pane" id="tab3">
+                        <h1>Privacy settings</h1>
+                    </div>
+                    <div class="tab-pane" id="tab4">
+                        <h1>Payments</h1>
+                    </div>
+                </div>	
+            </div>
+
+            <div class="dnl-ui-clearfix"></div>	
+        </div>
+
+    </div>
+
+
+</div>
+
+<!-- FOOTER -->
+<div class="container" id="footer">
+
+</div>
+</body>
 </html>
