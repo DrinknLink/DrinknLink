f6136357e6ab863f7e8576d703ea49074ae228a5
dsagud <dsagud@msn.com>
2013-06-12 00:02:40 +0200

    Uploaded basic page

--- DNL_yuchen/application/views/basic/body_basic.php
+++ DNL_yuchen/application/views/basic/body_basic.php
@@ -20,87 +20,92 @@
 
 <div class="container">
 
-    <div class="column column-1-1">
-        <div class="row">
-            <div id="navDiv" class="dnl-ui-drop-shadow dnl-ui-rounded-corners">								
-                <div>
-                    <h1>Navigation</h1>
+    <!-- NAVIGATION -->
+    <div id="basicNav" class="column">
 
-                    <div class="tabbable">
-                        <ul class="nav nav-tabs">                              
-                            <li class="bigNavLinks active">
-                                <a href="#tab1" data-toggle="tab">
-                                    <table>
-                                        <tr>
-                                            <td><i class="icon-cog"></i></td>
-                                            <td>&nbsp General</td>
-                                        </tr>
-                                    </table>
-                                </a>
-                            </li>
-                            <li class="bigNavLinks">
-                                <a href="#tab2" data-toggle="tab">
-                                    <table>
-                                        <tr>
-                                            <td><i class="icon-lock"></i></td>
-                                            <td>&nbsp Security</td>
-                                        </tr>
-                                    </table>
-                                </a>
-                            </li>
-                            <li class="bigNavLinks">
-                                <a href="#tab3" data-toggle="tab">
-                                    <table>
-                                        <tr>
-                                            <td><i class="icon-eye-open"></i></td>
-                                            <td>&nbsp Privacy</td>
-                                        </tr>
-                                    </table>
-                                </a>
-                            </li>
-                            <li class="bigNavLinks">
-                                <a href="#tab4" data-toggle="tab">
-                                    <table>
-                                        <tr>
-                                            <td><i class="icon-shopping-cart"></i></td>
-                                            <td>&nbsp Payments</td>
-                                        </tr>
-                                    </table> 
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>	
+        <div class="column column-1-1">
+            <div class="row">
+                <div id="navDiv" class="dnl-ui-drop-shadow dnl-ui-rounded-corners">								
+                    <div>
+                        <h1>Navigation</h1>
+
+                        <div class="tabbable">
+                            <ul class="nav nav-tabs">                              
+                                <li class="bigNavLinks active">
+                                    <a href="#tab1" data-toggle="tab">
+                                        <table>
+                                            <tr>
+                                                <td><i class="icon-cog"></i></td>
+                                                <td>&nbsp General</td>
+                                            </tr>
+                                        </table>
+                                    </a>
+                                </li>
+                                <li class="bigNavLinks">
+                                    <a href="#tab2" data-toggle="tab">
+                                        <table>
+                                            <tr>
+                                                <td><i class="icon-lock"></i></td>
+                                                <td>&nbsp Security</td>
+                                            </tr>
+                                        </table>
+                                    </a>
+                                </li>
+                                <li class="bigNavLinks">
+                                    <a href="#tab3" data-toggle="tab">
+                                        <table>
+                                            <tr>
+                                                <td><i class="icon-eye-open"></i></td>
+                                                <td>&nbsp Privacy</td>
+                                            </tr>
+                                        </table>
+                                    </a>
+                                </li>
+                                <li class="bigNavLinks">
+                                    <a href="#tab4" data-toggle="tab">
+                                        <table>
+                                            <tr>
+                                                <td><i class="icon-shopping-cart"></i></td>
+                                                <td>&nbsp Payments</td>
+                                            </tr>
+                                        </table> 
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>	
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="column column-1-2">				
-        <div class="row">
+        <div class="column column-1-2">				
+            <div class="row">
 
-            <div id="basicDiv" class="dnl-ui-drop-shadow dnl-ui-rounded-corners">								
-                <div class="tab-content eventsTabContent">
+                <div id="basicDiv" class="dnl-ui-drop-shadow dnl-ui-rounded-corners">								
+                    <div class="tab-content eventsTabContent">
 
-                    <div class="tab-pane active" id="tab1">
-                        <h1>General settings</h1>
-                    </div>
-                    <div class="tab-pane" id="tab2">
-                        <h1>Security settings</h1>
-                    </div>
-                    <div class="tab-pane" id="tab3">
-                        <h1>Privacy settings</h1>
-                    </div>
-                    <div class="tab-pane" id="tab4">
-                        <h1>Payments</h1>
-                    </div>
-                </div>	
+                        <div class="tab-pane active" id="tab1">
+                            <h1>General settings</h1>
+                        </div>
+                        <div class="tab-pane" id="tab2">
+                            <h1>Security settings</h1>
+                        </div>
+                        <div class="tab-pane" id="tab3">
+                            <h1>Privacy settings</h1>
+                        </div>
+                        <div class="tab-pane" id="tab4">
+                            <h1>Payments</h1>
+                        </div>
+                    </div>	
+                </div>
+
+                <div class="dnl-ui-clearfix"></div>	
             </div>
 
-            <div class="dnl-ui-clearfix"></div>	
         </div>
 
     </div>
-
+    <!-- END OF NAVIGATION -->
 
 </div>
 
